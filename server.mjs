35e117fe643c41252e1b9ad98e4e96603bb69a2c
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-10-02 00:37:06 +0500

    make other routes like 404 not found route

--- server.mjs
+++ server.mjs
@@ -14,9 +14,15 @@ const server = createServer((req, res) => {
     }else if (url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the About Page.');
-    } 
+    }  else if (url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Contact us at contact@example.com.');
+    }else {
+        // res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+})
     
-  });
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
