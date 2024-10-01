d86eb4c1e574fdcf392de4cb518371ba76b8054b
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-10-02 00:32:09 +0500

    make first route for homepage

--- server.mjs
+++ server.mjs
@@ -1,14 +1,18 @@
 import { createServer } from 'node:http';
 
-import { createServer } from 'node:http';
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    
+// This contains only the URL that is present in the actual HTTP request
+    const url = req.url;
+// first route
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the Home Page.');
+    }
+    
   });
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
