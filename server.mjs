a2346b31ec51ac4d3c23a8a2a415e9c2d5ec6fab
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-10-02 00:33:30 +0500

    make another route

--- server.mjs
+++ server.mjs
@@ -4,14 +4,17 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    
+
 // This contains only the URL that is present in the actual HTTP request
     const url = req.url;
 // first route
     if (url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the Home Page.');
-    }
+    }else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the About Page.');
+    } 
     
   });
   server.listen(port, hostname, () => {
